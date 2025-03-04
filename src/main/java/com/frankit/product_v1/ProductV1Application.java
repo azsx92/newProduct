88b4f22db343d5dd0597c3c1a8faf03943bd493e
azsx92 <azsx92@gmail.com>
2025-03-04 13:44:42 +0900

    first commit

---
+++ src/main/java/com/frankit/product_v1/ProductV1Application.java
@@ -0,0 +1,13 @@
+package com.frankit.product_v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductV1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/frankit/product_v1/ProductV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frankit.product_v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
